3d2c8756cdd93b4dca027ec5865a9151bbc4cbea
Vega <51040082+Abolfazl01110@users.noreply.github.com>
2023-11-28 00:57:13 +0330

    Update db_connect.php

--- PiriykaCheckoutAPI/config/db_connect.php
+++ PiriykaCheckoutAPI/config/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect('localhost', 'h200391_AE', 'Piriyka1qaz', 'h200391_piriyka', 3306);
+$conn = mysqli_connect('localhost', 'h200391_AE', 'root', 'databasename', 3306);
 mysqli_set_charset($conn, "utf8mb4");
 
 
